f10960da83de42a98ab4caa9871856a3fd501b75
Keagan Jarvis <keagan@thinkst.com>
2021-10-14 10:53:39 +0100

    Move tfstate into AzureStorage
    If other developers wanted to use this terraform repo. Also moves
    sensitive info out of local files.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,12 @@ terraform {
       version = "~>2.0"
     }
   }
+  backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "tfstate32554"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
 }
 provider "azurerm" {
   features {}
