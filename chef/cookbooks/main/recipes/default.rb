21a07784fb4ec014c47edbfd04ee8aff85077980
Keagan Jarvis <keagan@thinkst.com>
2021-11-08 10:57:38 +0000

    Get application running as service with https
    This commit includes the changes necessary to have the application
    running as a systemd managed service behind uwsgi. This change also
    includes the basics of getting https running on the application with
    nginx terminating the tls connection and forwarding the connection onto
    the uwsgi application. Initial certbot steps required for TLS
    certificates are also included. Both nginx and certbot setups will get
    expanded on.

--- chef/cookbooks/main/recipes/default.rb
+++ chef/cookbooks/main/recipes/default.rb
@@ -15,6 +15,23 @@ git "/web_app_feedback/" do
     action :sync
 end
 
+# TODO separate the next steps into a separate file/recipe
+
+# get nginx setup correctly before doing certbot stuff so that it auto fixes /etc/nginx/sites-enabled/application
+
+# certbot is recommended to use the snap install rather for 20.04
+# attempts at using `snap_package` was giving errors TODO say this?
+execute "Install certbot" do
+    command "snap install certbot --classic"
+end
+
+# run this command to get https: certbot --nginx -d web-app-feedback.space -d www.web-app-feedback.space --non-interactive --agree-tos -m webmaster@example.com
+# Note this command was interactive on first run through (added on --non-interactive --agree-tos -m webmaster@example.com).
+# Second note this requires the @.web-app-feedback.space domain A record to point to the public IP of the instance
+
+# create /etc/letsencrypt/renewal-hooks/post/ that restarts nginx
+
+# note avoided using the Python cookbooks, like Poise-python because they seem unmaintained
 execute "Create web_app_py_virtual_env" do
     command "virtualenv -p python3 /web_app_venv/"
 end
@@ -22,6 +39,49 @@ end
 execute "Install python libraries" do
     command "/web_app_venv/bin/pip install -r /web_app_feedback/web_app/requirements.txt"
 end
+
+
+# ensure to run ln -s /etc/nginx/sites-available/application /etc/nginx/sites-enabled when setting up nginx
+# and systemctl restart nginx afterwards
+# Should it use the nginx cookbook? If yes then use
+
+# necessary for uwsgi logging:
+directory '/var/log/uwsgi' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
+# install the uswgi service files in systemd,
+systemd_unit 'uwsgi-application.service' do
+    content({ Unit: {
+              Description: 'Feedback Web application UWSGI Workers',
+              After: 'network.target nginx.service',
+            },
+            Service: {
+              Type: 'notify',
+              ExecStart: '/web_app_venv/bin/uwsgi --die-on-term --ini /web_app_feedback/web_app/uwsgi.ini',
+              Restart: 'always',
+              WorkingDirectory: '/web_app_feedback/web_app/'
+            },
+            Install: {
+              WantedBy: 'multi-user.target',
+            } })
+    action [:create, :enable, :start] # TODO :start OR :restart ?
+    # user ubunutu TODO get correct user
+end
+
+
+
+
+
+
+
+
+
+###########
+#OLD:
 # execute "Install perl web framework" do
 #     command "cpanm Dancer2"
 # end
--- web_app/application_server.py
+++ web_app/application_server.py
@@ -6,12 +6,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_landing():
-    # return Comments.select().get().comment
     return render_template('landing_page.html')
 
 @app.route('/thanks', methods=['GET'])
 def get_thanks():
-    # return Comments.select().get().comment
     return render_template('thanks_page.html')
 
 @app.route('/submit_comment', methods=['POST'])
@@ -19,9 +17,20 @@ def submit_comment():
     if request.values.get("is_positive") != 'true' and request.values.get("is_positive") != 'false':
         return json.dumps({'result':'error'}), 400 # covers the case where someone is bypassing website to post to this route/end point.
     is_positive = request.values.get("is_positive") == 'true' # TODO JS request coming through on server as str, even though is bool in client obj
+    user_agent = request.headers.get('User-Agent')
     comment = request.values.get("comment")
-    Comments.create(comment=comment,is_positive=is_positive)
+    # Data validation would go here against `comment` to prevent injections, for now trusting the ORM
+    try:
+        Comments.create(comment=comment,is_positive=is_positive)
+    except: # blanket `except` for now
+        # TODO log the exception
+        return json.dumps({'result':'error'}), 400
     return json.dumps({'result':'success'}), 200
 
+
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=80) # TODO port 80 rework when using https
+    """
+    Main function, only necessary for quick testing/direct running, this does not get used when utilising
+    uwsgi and ngnix services/daemons
+    """
+    app.run(host= '0.0.0.0', port=80)
