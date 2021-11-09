79fd18a6cc075282ebc8e54e5faf63aff2ee9aeb
Keagan Jarvis <keagan@thinkst.com>
2021-11-09 15:07:21 +0000

    Pick up when user is not logged into Azure cli
    If the script to create a new server is run by a user that is not
    correctly logged into the Azure CLI, then the first `terraform init`
    call fails. This change picks up failures like this and directs the
    user how to log in.

--- create_new_server.sh
+++ create_new_server.sh
@@ -16,6 +16,8 @@ create_server()
     if [ "$#" -lt "1" ];then
         usage
     fi
+    # Would prefer to use Azure Key Vault or Hashicorp Vault to store secrets like the Asana API key
+    # but this is a quick hack that allows the Asana API key to get passed into the system
     ASANA_KEY="Default_unset_key"
     while getopts ":k:f" opt; do
         case $opt in
@@ -32,21 +34,27 @@ create_server()
         done
     git clone https://github.com/KeaganJarvis/web_app_feedback.git
     cd web_app_feedback/terraform
-    # az login # THIS IS NECESSARY IN THE CLOUD SHELL
-    terraform init
-    terraform apply -var="asana_key=${ASANA_KEY}" -auto-approve # TODO is it better to do a `plan` output to a file first then apply on that plan file?
+    if terraform init; then
+        echo "Successfully init'd the terrform state"
+        echo "Assuming this means the user is correctly logged into azure CLI."
+        echo "Proceeding."
+    else
+        echo "ERROR"
+        echo "Could not init the terraform state"
+        echo "Most likely issue is the current user is not logged into the Azure CLI"
+        echo "Please run 'az login' and follow the steps output to get logged in"
+        exit 1
+    fi
+    terraform apply -var="asana_key=${ASANA_KEY}" -auto-approve
     terraform apply -var="asana_key=${ASANA_KEY}" -target azurerm_linux_virtual_machine.web_app_mvp_vm -auto-approve
     # TODO the randomly assigned public IP is only displayable after this ^ second `terraform apply` is run
     terraform output -raw tls_private_key
-    echo "Successully created server, you can access it using the private key above^"
-
+    echo "Successully created server, you can get shell access to it using the private key above^"
     echo "The server has the following public ip:"
-    # terraform apply -target=output.public_ip_address
     terraform output public_ip_address # this is the line that makes the double `terraform apply`s necessary above
     echo "The web site should be available via http on that IP after approximately 5 mins"
     echo "FINISHED"
 
-    # TODO comment stating that would prefer to use Azure Key Vault or Hashicorp Vault to store secrets like the Asana API key but this is a quick hack
 }
 
 create_server "$@" 2>&1
